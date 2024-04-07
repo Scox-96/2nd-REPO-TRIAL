20828aa0e3ea98b068169db2d6ce0af048f360d5
scox-96 <scox0315@icloud.com>
2024-04-06 22:17:33 -0600

    turn LED on every fifty ms

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,14 +1,23 @@
 #include <Arduino.h>
 
-
+#define ledPin 8
+int delayTime = 50;
 void setup() {
-  // put your setup code here, to run once:
+
+
+
+
+  pinMode(ledPin, OUTPUT);
 }
 
-// Hi this is cool
+
 
 
 void loop() {
+  digitalWrite(ledPin, HIGH); // sets the LED on
+  delay(delayTime);                // waits for a second
+  digitalWrite(ledPin, LOW);  // sets the LED off
+  delay(delayTime);             
   // put your main code here, to run repeatedly:
 }
 
