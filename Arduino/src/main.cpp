cd977bc34543b12e5fc414e6ee8b512215199bbf
scox-96 <scox0315@icloud.com>
2024-04-07 00:25:13 -0600

    Turn on LED with button

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,23 +1,34 @@
 #include <Arduino.h>
 
-#define ledPin 8
-int delayTime = 50;
+const int buttonPin = 1;  // the number of the pushbutton pin
+const int ledPin = 7;    // the number of the LED pin
+int buttonState;  // variable for reading the pushbutton status
+
 void setup() {
-
-
-
-
+  
+  // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+
 }
 
-
-
-
 void loop() {
-  digitalWrite(ledPin, HIGH); // sets the LED on
-  delay(delayTime);                // waits for a second
-  digitalWrite(ledPin, LOW);  // sets the LED off
-  delay(delayTime);             
-  // put your main code here, to run repeatedly:
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == LOW) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } 
+  else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+
+  }
+
+delay(50);
+
 }
 
